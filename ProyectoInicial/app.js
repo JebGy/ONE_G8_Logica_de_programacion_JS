ada4bb33dbec59f7b823434ce6b0b99d17e54afd
JebGy <martinmunayco123@hotmail.com>
2025-01-15 16:29:10 -0500

    Completado 4

--- ProyectoInicial/app.js
+++ ProyectoInicial/app.js
@@ -1,3 +1,34 @@
-alert("Hello World");
-let numeroUsuario = prompt("Me indicas un número porfavor:");
-console.log(numeroUsuario);
+//Variables
+let numeroSecreto = Math.floor(Math.random()*10)+1
+let numeroUsuario = 0
+let intentos = 1;
+let limitedeIntentos = 5;
+
+
+while (numeroSecreto != numeroUsuario) {
+    numeroUsuario = prompt("Me indicas un número entre el 1 y el 10 porfavor:");
+
+    console.log(numeroUsuario);
+
+    if (numeroUsuario == numeroSecreto) {
+        alert(`¡Acertaste, el número era ${numeroSecreto}. Usaste ${intentos} ${intentos==1?"intento":"intentos"}!`);
+    } else {
+        alert("Perdón, no acertaste :(");
+        intentos++;
+        if (numeroUsuario == 0) {
+            alert("El 0 no vale pillín!")
+        }
+        if (numeroUsuario > numeroSecreto) {
+            alert("El número secreto es menor :)");
+        }
+        if (numeroUsuario < numeroSecreto) {
+            alert("El número secreto es mayor :)");
+        }
+        if (intentos > limitedeIntentos) {
+            alert(`Máximo de inentos ${limitedeIntentos} alcanzado!`)
+            break;
+        }
+    }
+
+
+}
